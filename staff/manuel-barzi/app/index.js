2ab984a02a5f19a3918f991ff606b953baeeb43e
manuelbarzi <manuelbarzi@gmail.com>
2023-03-20 23:09:59 +0100

    add first steps on behavior with js for the app

--- staff/manuel-barzi/app/index.js
+++ staff/manuel-barzi/app/index.js
@@ -1,4 +1,32 @@
 console.log('hello world')
 
-document.querySelector('.register').style.display = 'none'
-document.querySelector('.home').style.display = 'none'
+var loginPage = {}
+var registerPage = {}
+var homePage = {}
+
+loginPage.container = document.querySelector('.login')
+registerPage.container = document.querySelector('.register')
+homePage.container = document.querySelector('.home')
+
+loginPage.container.style.display = 'none'
+registerPage.container.style.display = 'none'
+homePage.container.style.display = 'none'
+
+loginPage.container.style.display = 'flex'
+
+loginPage.container.querySelector('a').onclick = function(event) {
+    event.preventDefault()
+
+    loginPage.container.style.display = 'none'
+    registerPage.container.style.display = 'flex'
+}
+
+registerPage.container.querySelector('a').onclick = function(event) {
+    event.preventDefault()
+
+    registerPage.container.style.display = 'none'
+    loginPage.container.style.display = 'flex'
+}
+
+// TODO learn about .onsubmit
+// TODO apply .onsubmit in loginPage form, and when email === 'peter@pan.com' and password === '123123123', hide loginPage container and show homePage container
