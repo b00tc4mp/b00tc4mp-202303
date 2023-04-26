d61b62f4117a915e688819298f53ea299c8c3dd4
manuelbarzi <manuelbarzi@gmail.com>
2023-04-26 23:18:46 +0200

    mechanize login process in app with react

---
+++ staff/manuel-barzi/web/app/src/pages/Login.jsx
@@ -0,0 +1,31 @@
+import { authenticateUser } from '../logic'
+
+export default function Login(props) {
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        const email = event.target.email.value
+        const password = event.target.password.value
+
+        try {
+            authenticateUser(email, password)
+
+            props.onAuthenticated()
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+    return <div className="login page">
+        <h1>login</h1>
+
+        <form className="form" onSubmit={handleSubmit}>
+            <input className="input" type="email" name="email" placeholder="email"></input>
+            <input className="input" type="password" name="password" placeholder="password"></input>
+
+            <button className="button">login</button>
+        </form>
+
+        <p>Go to <a href="">register</a></p>
+    </div>
+}
